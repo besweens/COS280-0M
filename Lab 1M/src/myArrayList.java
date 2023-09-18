cd5cb2822b39e371fc702b2df07e0b6b455f8361
besweens <144698652+besweens@users.noreply.github.com>
2023-09-18 10:18:10 -0400

    src update

---
+++ Lab 1M/src/myArrayList.java
@@ -0,0 +1,14 @@
+/*
+ * CLASS myArrayList
+ * Create fraction objects with numerator and denominator inputs.
+ * Perform arithmetic with fraction objects that behaves like traditional arithmetic
+ * 
+ * @author Ben Sweeney,Jere Parisic, Abby Pitcairn, Esther Mugish
+ * 
+ * @version 20230917
+ * 
+ */
+
+public class myArrayList {
+
+}
